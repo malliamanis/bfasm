a253064c16daac20c8668b28979ad0b4249f8bdb
malliamanis <malliamanisgit@gmail.com>
2024-04-19 22:38:31 +0300

    Add reading command line arguments

--- src/main.asm
+++ src/main.asm
@@ -3,21 +3,59 @@ default rel
 global _start
 
 section .data
-str: db "Hello World!", 10
+size: dq 30000
+newline: db 10
+
+section .bss
+cells: resq 1
+pointer: resq 1
 
 section .text
+
+; prints null-terminated strings
+print: ; rdi contains the string
+	mov rsi, rdi
+
+print_loop:
+	; print character
+	mov rax, 1
+	mov rdi, 1
+	mov rdx, 1
+	syscall
+
+	; increment pointer and check if the char is 0
+	inc rsi
+	mov al, byte [rsi]
+	cmp al, 0
+	jne print_loop
+
+	ret
+
 _start:
 	push rbp
 	mov rbp, rsp
 
+	mov r8, qword [rsp + 8] ; argc
+
+	xor r9, 0 ; iterator
+arg_loop:
+	mov rdi, qword [rsp + 16 + 8 * r9] ; argv[iterator]
+	call print
+
 	mov rax, 1
 	mov rdi, 1
-	lea rsi, [str]
-	mov rdx, 13
+	lea rsi, [newline]
+	mov rdx, 1
 	syscall
 
+	inc r9
+	cmp r9, r8
+
+	jne arg_loop
+
+	; quit
 	pop rbp
 
 	mov rax, 60
-	xor rdi, rdi
+	mov rdi, 0
 	syscall
